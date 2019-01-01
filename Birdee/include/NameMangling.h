5f74dd13533ea495b2fae318a0d9d25b18168eaf
Menooker <myjisgreat@live.cn>
2019-01-01 22:37:02 +0800

    MangleNameAndAppend now processes template strings correctly

--- Birdee/include/NameMangling.h
+++ Birdee/include/NameMangling.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <string>
+#include <sstream>
+#include <iomanip>
 
 /*Meanings of "_X"
 __ - _
@@ -10,6 +12,16 @@ _3 - ]
 */
 namespace Birdee
 {
+	//Copied from https://stackoverflow.com/a/33447587/4790873
+	//User: AndreyS Scherbakov
+	template <typename I> std::string n2hexstr(I w, size_t hex_len = sizeof(I) << 1) {
+		static const char* digits = "0123456789ABCDEF";
+		std::string rc(hex_len, '0');
+		for (size_t i = 0, j = (hex_len - 1) * 4; i < hex_len; ++i, j -= 4)
+			rc[i] = digits[(w >> j) & 0x0f];
+		return rc;
+	}
+
 	inline void MangleNameAndAppend(std::string& out, const std::string& name)
 	{
 		out.reserve(out.size() + name.size()*1.2);
@@ -40,9 +52,15 @@ namespace Birdee
 				out += '_';
 				out += '4';
 			}
-			else
+			else if (isalnum(name[i])) //is number of [a-z][A-Z]
 			{
 				out += name[i];
+			}
+			else
+			{
+				out += '_';
+				out += 'x';
+				out += n2hexstr((char)name[i]);
 			}
 		}
 	}
